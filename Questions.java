fe538bc2f3a751b685d7ad2decaff90e643571a8
Gabriel <0358929@gamil.com>
2025-03-06 09:12:51 -0500

    Initial sharing of project

---
+++ Questions.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Questions here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Questions
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    /*
+     * 1) Pressing enter still does the same when typing anything
+     * 2)
+     * 3)The toffset allows you to start anywhere you want within the string
+     * 4)It's a boolean that ends with a specified suffix. It's called endsWith
+     * 5)length() and it has no paramaters only the string its self
+     * 6)I used control F/f3, and it was easy for the first one, the second one was a little difficult since
+     * length is a parameter its self
+     * 7)trim() | text.trim()
+     * 8)
+     * 9)
+     */
+    
+    /**
+     * Constructor for objects of class Questions
+     */
+    public Questions()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
